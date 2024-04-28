9c2e3ddfba9a95e0f168e7675180c7da8c0adbe1
pilipchu <114575426+pilipchu@users.noreply.github.com>
2024-04-28 19:43:33 +0300

    add

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,7 +2,6 @@ import FriendListItem from "./FriendListItem"
 import css from "./FriendList.module.css";
 function FriendList ({friends}) {
     return (
-        <div className={css.conteiner}>
               <ul className={css.list}>
 	{friends.map((friend)=> {return <li className={css.line} key={friend.id}>
         <FriendListItem name={friend.name}
@@ -10,7 +9,6 @@ function FriendList ({friends}) {
         isOnline={friend.isOnline}/>
     </li>})}
 </ul>
-        </div>
     )    
 }
 export default FriendList
